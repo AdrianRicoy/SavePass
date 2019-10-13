f9245780c16dac957631490d630a6ab24bdfa938
Adrián <noe.ricoy@hotmail.com>
2019-10-13 02:44:51 -0500

    Se ha cambiado el diseño y la lógica a la vista

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,9 +36,15 @@
             this.label4 = new System.Windows.Forms.Label();
             this.txtUser = new System.Windows.Forms.TextBox();
             this.txtPass = new System.Windows.Forms.TextBox();
-            this.BtnGenerate = new System.Windows.Forms.Button();
+            this.BtnRandom = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lstData = new System.Windows.Forms.ListView();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnDelete = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -93,7 +99,7 @@
             this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAdd.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.btnAdd.Location = new System.Drawing.Point(703, 19);
+            this.btnAdd.Location = new System.Drawing.Point(703, 109);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(95, 37);
             this.btnAdd.TabIndex = 1;
@@ -105,7 +111,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(297, 19);
+            this.label4.Location = new System.Drawing.Point(297, 109);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(279, 25);
             this.label4.TabIndex = 2;
@@ -114,7 +120,7 @@
             // txtUser
             // 
             this.txtUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUser.Location = new System.Drawing.Point(283, 56);
+            this.txtUser.Location = new System.Drawing.Point(283, 146);
             this.txtUser.Name = "txtUser";
             this.txtUser.Size = new System.Drawing.Size(182, 24);
             this.txtUser.TabIndex = 3;
@@ -122,30 +128,31 @@
             // txtPass
             // 
             this.txtPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPass.Location = new System.Drawing.Point(471, 56);
+            this.txtPass.Location = new System.Drawing.Point(471, 146);
             this.txtPass.Name = "txtPass";
             this.txtPass.Size = new System.Drawing.Size(185, 24);
             this.txtPass.TabIndex = 4;
             // 
-            // BtnGenerate
+            // BtnRandom
             // 
-            this.BtnGenerate.BackColor = System.Drawing.Color.Teal;
-            this.BtnGenerate.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.BtnGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnGenerate.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.BtnGenerate.Location = new System.Drawing.Point(703, 71);
-            this.BtnGenerate.Name = "BtnGenerate";
-            this.BtnGenerate.Size = new System.Drawing.Size(95, 37);
-            this.BtnGenerate.TabIndex = 5;
-            this.BtnGenerate.Text = "!\"#!";
-            this.BtnGenerate.UseVisualStyleBackColor = false;
+            this.BtnRandom.BackColor = System.Drawing.Color.Teal;
+            this.BtnRandom.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnRandom.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnRandom.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.BtnRandom.Location = new System.Drawing.Point(703, 161);
+            this.BtnRandom.Name = "BtnRandom";
+            this.BtnRandom.Size = new System.Drawing.Size(95, 37);
+            this.BtnRandom.TabIndex = 5;
+            this.BtnRandom.Text = "!\"#!";
+            this.BtnRandom.UseVisualStyleBackColor = false;
+            this.BtnRandom.Click += new System.EventHandler(this.BtnRandom_Click);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Teal;
-            this.label5.Location = new System.Drawing.Point(278, 140);
+            this.label5.Location = new System.Drawing.Point(278, 230);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(519, 25);
             this.label5.TabIndex = 6;
@@ -155,29 +162,96 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(283, 131);
+            this.label6.Location = new System.Drawing.Point(283, 221);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(57, 25);
             this.label6.TabIndex = 7;
             this.label6.Text = "Data";
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Transparent;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.Red;
+            this.btnClose.Location = new System.Drawing.Point(760, 2);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(37, 34);
+            this.btnClose.TabIndex = 8;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // lstData
+            // 
+            this.lstData.BackColor = System.Drawing.SystemColors.Control;
+            this.lstData.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lstData.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader1,
+            this.columnHeader2});
+            this.lstData.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lstData.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.lstData.HideSelection = false;
+            this.lstData.Location = new System.Drawing.Point(288, 258);
+            this.lstData.Name = "lstData";
+            this.lstData.Size = new System.Drawing.Size(500, 180);
+            this.lstData.TabIndex = 9;
+            this.lstData.UseCompatibleStateImageBehavior = false;
+            this.lstData.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.DisplayIndex = 2;
+            this.columnHeader3.Width = 0;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.DisplayIndex = 0;
+            this.columnHeader1.Text = "User";
+            this.columnHeader1.Width = 250;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.DisplayIndex = 1;
+            this.columnHeader2.Text = "Password";
+            this.columnHeader2.Width = 250;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Teal;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnDelete.Location = new System.Drawing.Point(702, 209);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(95, 37);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
             // 
             // Index
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.lstData);
+            this.Controls.Add(this.btnClose);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.BtnGenerate);
+            this.Controls.Add(this.BtnRandom);
             this.Controls.Add(this.txtPass);
             this.Controls.Add(this.txtUser);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.panel1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Index";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SavePass";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -196,9 +270,15 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txtUser;
         private System.Windows.Forms.TextBox txtPass;
-        private System.Windows.Forms.Button BtnGenerate;
+        private System.Windows.Forms.Button BtnRandom;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ListView lstData;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button btnDelete;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -14,24 +14,77 @@ namespace SavePass
     {
         public Index()
         {
-            if(Validate())
-            {
-                InitializeComponent();
-            }
+            InitializeComponent();
+            GetDataUserPass();
         }
 
         /// <summary>
         /// Obtener los labels con la información del usuario, contraseña
         /// </summary>
-        private void GetLabelsUserPass()
+        private void GetDataUserPass()
         {
+            List<String> lstUserPasword = new HandleUserPass().GetDataPassAndUser();
             
+
+            foreach (String user in lstUserPasword)
+            {
+                ListViewItem list = new ListViewItem();
+                list.SubItems.Add(user.Split(';')[0]);
+                
+                try
+                {
+                    list.SubItems.Add(user.Split(';')[1]);
+                } catch(Exception e) { list.SubItems.Add("Nop"); }
+
+                list.SubItems.Add("");
+                lstData.Items.Add(list);
+            } 
         }
 
         private void BtnAdd_Click(object sender, EventArgs e)
         {
             string pass = txtPass.Text;
             string user = txtUser.Text;
+
+            HandleUserPass handleUser = new HandleUserPass();
+            
+            if(handleUser.AddUserPass(user, pass))
+            {
+                ListViewItem list = new ListViewItem();
+                list.SubItems.Add(user);
+                list.SubItems.Add(pass);
+                list.SubItems.Add("");
+                lstData.Items.Add(list);
+            }
+        }
+        /// <summary>
+        /// Button para cerrar el formulario
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        /// <summary>
+        /// Genera una constraseña aleatoria para el usuario
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnRandom_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            Random random = new Random();
+
+            int lim = random.Next(15, 20);
+
+            for(int i = 0; i < lim; i++)
+            {
+                int n = random.Next(33, 160);
+                result += (char)n;
+            }
+
+            txtPass.Text = result;
         }
     }
 }
